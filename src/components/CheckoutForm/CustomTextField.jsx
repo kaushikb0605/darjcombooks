dc86b9d325c99cf6032b8b5f62e6525a318c356c
Kaushik Barua <kbarua21@gmail.com>
2021-07-10 02:59:06 +0530

    first-commit

---
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { TextField,Grid } from '@material-ui/core'
+import { useFormContext,Controller } from 'react-hook-form'
+
+const FormInput = ({name,label}) => {
+    const {control}=useFormContext();
+    const isError = false;
+    return (
+        <Grid item xs={12} sm={6}>
+            <Controller 
+                 render = {({ field})=> (
+                <TextField
+                defaultValue=""
+                control={control}
+                fullWidth
+                name={name}
+                label={label}
+                required
+                error={isError}
+                />
+            )}
+            />
+        </Grid>
+    );
+}
+
+export default FormInput;
+
